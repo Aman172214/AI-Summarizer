1ac09ba97019141a87902e2e2f5da24947b54548
Aman <Sharmaamqn@gmail.com>
2023-11-17 16:25:01 +0530

    display the summary and add logic

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { copy, linkIcon, tick, submitIcon } from "../assets";
+import { copy, linkIcon, loader, submitIcon } from "../assets";
 import { useLazyGetSummaryQuery } from "../services/article";
 
 const Content = () => {
   const [article, setArticle] = useState({ url: "", summary: "" });
-  const [allArticles, setAllArticles] = useState([]);
+  const [allURLs, setAllURLs] = useState([]);
   const [getSummary, { isLoading, error }] = useLazyGetSummaryQuery();
 
   useEffect(() => {
-    const storedArticles = JSON.parse(localStorage.getItem("articles"));
+    const storedURLs = JSON.parse(localStorage.getItem("articles"));
 
-    if (storedArticles) {
-      setAllArticles(storedArticles);
+    if (storedURLs) {
+      setAllURLs(storedURLs);
     }
   }, []);
 
@@ -24,18 +24,19 @@ const Content = () => {
     const { data } = await getSummary({ articleUrl: article.url });
     if (data?.summary) {
       const newArticle = { ...article, summary: data.summary };
-      const newAllArticles = [newArticle, ...allArticles];
+      const newAllURLs = [newArticle, ...allURLs];
 
       setArticle(newArticle);
-      setAllArticles(newAllArticles);
+      setAllURLs(newAllURLs);
 
-      localStorage.setItem("articles", JSON.stringify(newAllArticles));
+      localStorage.setItem("articles", JSON.stringify(newAllURLs));
     }
   };
 
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="w-full flex flex-col gap-2">
+        {/* Search Form */}
         <form
           onSubmit={submitHandler}
           className="relative flex justify-center items-center"
@@ -59,7 +60,58 @@ const Content = () => {
             <img src={submitIcon} alt="submit_icon" />
           </button>
         </form>
-        
+        {/* Display URLs */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allURLs.map((article, index) => (
+            <div
+              key={index}
+              onClick={() => setArticle(article)}
+              className="link_card"
+            >
+              <div className="copy_btn">
+                <img
+                  src={copy}
+                  alt="copy_icon"
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {article.url}
+              </p>
+            </div>
+          ))}
+        </div>
+        {/* Display Summary */}
+        <div className="my-10 max-w-full flex justify-center items-center">
+          {isLoading ? (
+            <img
+              src={loader}
+              alt="loader"
+              className="w-20 h-20 object-contain"
+            />
+          ) : error ? (
+            <p className="font-inter font-bold text-black text-center">
+              Sorry, something went wrong!
+              <br />
+              <span className="font-satoshi font-normal text-gray-700">
+                {error?.data?.error}
+              </span>
+            </p>
+          ) : (
+            article.summary && (
+              <div className="flex flex-col gap-3">
+                <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                  Article <span className="blue_gradient">Summary</span>
+                </h2>
+                <div className="summary_box">
+                  <p className="font-inter font-medium text-sm text-gray-700">
+                    {article.summary}
+                  </p>
+                </div>
+              </div>
+            )
+          )}
+        </div>
       </div>
     </section>
   );
